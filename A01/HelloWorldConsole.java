e9af7954636bc5d481020cef127f61c66010fa96
Ali Ariff <ali.ariff12@gmail.com>
2017-04-26 20:05:55 +0200

    beautify format

--- A01/HelloWorldConsole.java
+++ A01/HelloWorldConsole.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class HelloWorldConsole {
 
   public static void main(String[] args) {
-    HashMap<String,String[]> supportedLanguages = new HashMap<String,String[]>();
+    HashMap<String, String[]> supportedLanguages = new HashMap<String, String[]>();
     supportedLanguages.put("d", new String[] { "deutsch", "Guten Tag!" });
     supportedLanguages.put("e", new String[] { "english", "Good Day!" });
     supportedLanguages.put("f", new String[] { "francais", "Bonne journée!" });
@@ -13,12 +13,12 @@ public class HelloWorldConsole {
     String input, format, key;
     String[] value, language;
 
-    while(true) {
+    while (true) {
       System.out.println("Select one of the following:");
       for (HashMap.Entry<String, String[]> entry : supportedLanguages.entrySet()) {
         key = entry.getKey();
         value = entry.getValue();
-            format = String.format("\t[%s]%s", key.toUpperCase(), value[0].substring(1));
+        format = String.format("\t[%s]%s", key.toUpperCase(), value[0].substring(1));
         System.out.println(format);
       }
       System.out.println("[Q]uit");
