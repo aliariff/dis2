7670d0b1a4cbfeac8090c0c43de8fff4d2e2aea8
Ali Ariff <ali.ariff12@gmail.com>
2017-05-08 16:47:09 +0200

    add attr for simple window

--- A02/Code/SimpleWindow.java
+++ A02/Code/SimpleWindow.java
@@ -1,3 +1,45 @@
 public class SimpleWindow {
+  private int startX, startY, width, height;
+  private String name;
+
+  public int getStartX() {
+    return startX;
+  }
+
+  public void setStartX(int startX) {
+    this.startX = startX;
+  }
+
+  public int getStartY() {
+    return startY;
+  }
+
+  public void setStartY(int startY) {
+    this.startY = startY;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 
 }
