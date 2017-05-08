c12db9ba87be69856d1851ea433a93263be47bdd
Ali Ariff <ali.ariff12@gmail.com>
2017-05-08 16:49:10 +0200

    call drawline in my app

--- A02/Code/MyApp.java
+++ A02/Code/MyApp.java
@@ -1,11 +1,8 @@
-import java.awt.geom.Point2D;
-
 public class MyApp {
 
   public static void main(String[] args) {
     WindowSystem ws = new WindowSystem(700, 700);
-    ws.setStartPoint(new Point2D.Float(0.2f, 0.3f));
-    ws.setEndPoint(new Point2D.Float(0.8f, 0.7f));
+    ws.drawLine(0.2f, 0.3f, 0.8f, 0.7f);
   }
 
 }
--- A02/Code/WindowSystem.java
+++ A02/Code/WindowSystem.java
@@ -1,29 +1,12 @@
 import java.awt.Color;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import de.rwth.hci.Graphics.GraphicsEventSystem;
 
 public class WindowSystem extends GraphicsEventSystem {
   private int width, height;
+  private int StartX, StartY, EndX, EndY;
   private ArrayList<SimpleWindow> simpleWindows;
-  private Point2D startPoint, endPoint;
-
-  public Point2D getEndPoint() {
-    return endPoint;
-  }
-
-  public void setEndPoint(Point2D endPoint) {
-    this.endPoint = endPoint;
-  }
-
-  public Point2D getStartPoint() {
-    return startPoint;
-  }
-
-  public void setStartPoint(Point2D startPoint) {
-    this.startPoint = startPoint;
-  }
 
   public WindowSystem(int i, int j) {
     super(i, j);
@@ -34,16 +17,15 @@ public class WindowSystem extends GraphicsEventSystem {
 
   @Override
   protected void handlePaint() {
-    if (this.startPoint != null && this.endPoint != null) {
-      this.setColor(Color.RED);
-      this.drawLine((float) this.startPoint.getX(), (float) this.startPoint.getY(),
-          (float) this.endPoint.getX(), (float) this.endPoint.getY());
-    }
+    this.setColor(Color.RED);
+    super.drawLine(StartX, StartY, EndX, EndY);
   }
 
   public void drawLine(float StartX, float StartY, float EndX, float EndY) {
-    super.drawLine(StartX * this.width, StartY * this.height, EndX * this.width,
-        EndY * this.height);
+    this.StartX = (int) (StartX * this.width);
+    this.StartY = (int) (StartY * this.height);
+    this.EndX = (int) (EndX * this.width);
+    this.EndY = (int) (EndY * this.height);
   }
 
 }
