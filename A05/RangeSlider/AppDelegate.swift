aff2998cad84bd12dbdba07729280734631dc399
Ali Ariff <ali.ariff12@gmail.com>
2017-06-16 22:52:44 +0200

    clean init

---
+++ A05/RangeSlider/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RangeSlider
+//
+//  Created by Ali Ariff on 6/16/17.
+//  Copyright © 2017 Ali Ariff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
