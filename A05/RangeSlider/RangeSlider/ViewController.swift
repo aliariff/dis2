7992ee87c8cc60f278f770d8f30270e92f0d7a00
Ali Ariff <ali.ariff12@gmail.com>
2017-06-16 22:41:30 +0200

    init range slider

---
+++ A05/RangeSlider/RangeSlider/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RangeSlider
+//
+//  Created by Ali Ariff on 6/16/17.
+//  Copyright © 2017 Ali Ariff. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
