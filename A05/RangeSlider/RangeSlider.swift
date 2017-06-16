d1c56443217439a2807447cea845d84ef2748221
Ali Ariff <ali.ariff12@gmail.com>
2017-06-16 23:23:02 +0200

    create RangeSlider

---
+++ A05/RangeSlider/RangeSlider.swift
@@ -0,0 +1,13 @@
+//
+//  RangeSlider.swift
+//  RangeSlider
+//
+//  Created by Ali Ariff on 6/16/17.
+//  Copyright © 2017 Ali Ariff. All rights reserved.
+//
+
+import Cocoa
+
+class RangeSlider: NSView {
+    
+}
