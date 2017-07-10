3f9c76914eb24452d7cfaef7690d2313ffa51fdb
Ali Ariff <ali.ariff12@gmail.com>
2017-07-10 23:28:41 +0200

    update

--- final/index.js
+++ final/index.js
@@ -1,56 +1,43 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var path = require('path');
 var os = require('os');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-// var configLoader = require('config-json');
-
-// configLoader.load('./config.json');
-
-var intervalObj;
-// var config = configLoader.get();
+var cache_images = {};
 var config = {
     port: 1234
 };
-var process;
-var isStreaming = false;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// io.origins(config['client-host'] + ':' + config['client-port']);
-
 app.get('/', function (req, res) {
-  res.send('Hello From Fridge' + os.hostname())
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.post('/image', function(req, res) {
-  sendImage(req.body.image);
-  res.send('Image Received');
-});
-
-http.listen(config['port'], function() {
-  console.log('listening on *:' + config['port']);
+    sendImage(req.body);
+    res.send('Image Received');
 });
 
 io.on('connection', function(socket) {
-    socket.on('start-stream', function() {
-        if (!isStreaming) {
-            startStreaming();
-        } else {
-            sendImage();
-        }
-    });
-
-    socket.on('disconnect', function() {
-        if (io.engine.clientsCount == 0) {
-            stopStreaming();
-        }
-    });
+    for(key in cache_images) {
+        socket.emit('live-stream', {
+            cam_id: key,
+            image_base64: cache_images[key]
+        });
+    }
 });
 
-function sendImage(image) {
-    //console.log('sending image');
-    io.sockets.emit('live-stream', image);
+http.listen(config['port'], function() {
+    console.log('listening on *:' + config['port']);
+});
+
+function sendImage(data) {
+    if (data.cam_id && data.image_base64) {
+        cache_images[data.cam_id] = data.image_base64;
+        io.sockets.emit('live-stream', data);
+    }
 }
